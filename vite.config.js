25c8d037989678c91cf06e947add10c951201b87
alejandro-diaz-barea <126179563+alejandro-diaz-barea@users.noreply.github.com>
2024-12-02 08:46:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  cors:{
+    server: {
+      proxy: {
+        '/login': {
+          target: 'https://request-flaskapi-qx9g.onrender.com',
+          changeOrigin: true,
+          secure: false,
+        },
+        '/refresh': {
+          target: 'https://request-flaskapi-qx9g.onrender.com',
+          changeOrigin: true,
+          secure: false,
+        },
+        '/api': {
+          target: 'https://request-flaskapi-qx9g.onrender.com',
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+
+  }
+
+});
